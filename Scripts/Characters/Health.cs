2ae474dc3192b9bc6395f765cfba0244cfc8d9dc
Dmitry Polovkov <sonics.scs@gmail.com>
2025-05-04 13:54:17 +0300

    Add Health

---
+++ Scripts/Characters/Health.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float _maxHealth;
+    [SerializeField] private float _invulnerabilityTime = 1f;
+
+    /*[SerializeField] private PlayerAnimator _animator;*/
+    [SerializeField] private AudioClip _damageSound;
+
+    private float _currentHeadth;
+    private float _invulnerabilityTimer;
+
+    private bool _isInvulnerability;
+    private bool _isDie;
+    private bool _isTakeDamage;
+
+    private void Awake()
+    {
+        _currentHeadth = _maxHealth;
+    }
+
+    private void TakeDamage(float damage)
+    {
+        if (_isInvulnerability)
+        {
+            return;
+        }
+
+        _currentHeadth -= damage;
+        /*_animator.TakeDamageAnimator(_isTakeDamage);*/
+        AudioSource.PlayClipAtPoint(_damageSound, transform.position);
+        
+        if (_currentHeadth <= 0)
+        {
+            Die();
+        }
+        else
+        {
+            _isInvulnerability = true;
+            _invulnerabilityTimer = _invulnerabilityTime;
+        }
+    }
+
+    private void Die()
+    {
+        /*_animator.DieAnimation(_isDie);*/
+        Destroy(gameObject);
+    }
+}
